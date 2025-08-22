43e73f7a943e36edddd85a54c6eeac92834e71ec
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-08-23 00:42:12 +0200

    create task objective to start a new task, resume a current task and hand it in when compelte

---
+++ src/classes/TaskObjectiveClass.ts
@@ -0,0 +1,107 @@
+import { getMaps } from '../api_calls/Maps';
+import { actionAcceptNewTask, actionCompleteTask } from '../api_calls/Tasks';
+import { ObjectiveTargets } from '../types/ObjectiveData';
+import { DestinationSchema } from '../types/types';
+import { logger, sleep } from '../utils';
+import { Character } from './CharacterClass';
+import { ApiError } from './ErrorClass';
+import { Objective } from './ObjectiveClass';
+
+export class MonsterTaskObjective extends Objective {
+  character: Character;
+  type: 'monster';
+
+  constructor(character: Character) {
+    super(`task_1_monstertask`, 'not_started');
+
+    this.character = character;
+  }
+
+  async execute(): Promise<boolean> {
+    // "task": "yellow_slime",
+    // "task_type": "monsters",
+    // "task_progress": 94,
+    // "task_total": 157,
+
+    if (this.character.data.task === '') {
+      this.startNewTask();
+    }
+
+    const maps = (await getMaps(this.character.data.task, 'monster')).data;
+
+    if (maps.length === 0) {
+      logger.error(`Cannot find the task target. This shouldn't happen ??`);
+      return;
+    }
+
+    const contentLocation = this.character.evaluateClosestMap(maps);
+
+    await this.character.move({ x: contentLocation.x, y: contentLocation.y });
+
+    await this.character.fight(
+      this.character.data.task_total - this.character.data.task_progress,
+      this.character.data.task,
+    );
+
+    if (
+      this.character.data.task_total - this.character.data.task_progress ===
+      0
+    ) {
+      logger.info(
+        `Completed ${this.character.data.task_total} fights. Handing in task`,
+      );
+      const maps = (await getMaps('monsters', 'tasks_master')).data;
+
+      // ToDo: I have this if a few times. I could put it into the getMaps() function
+      if (maps.length === 0) {
+        logger.error(`Cannot find the tasks master. This shouldn't happen ??`);
+        return;
+      }
+
+      const contentLocation = this.character.evaluateClosestMap(maps);
+
+      await this.character.move({ x: contentLocation.x, y: contentLocation.y });
+
+      const response = await actionCompleteTask(this.character.data);
+
+      if (response instanceof ApiError) {
+        if (response.error.code === 499) {
+          logger.warn(
+            `Character is in cooldown. [Code: ${response.error.code}]`,
+          );
+          await sleep(this.character.data.cooldown, 'cooldown');
+        }
+        // ToDo: Handle this somehow
+      } else {
+        this.character.data = response.data.character;
+      }
+    }
+
+    return true;
+  }
+
+  async startNewTask() {
+    const maps = (await getMaps('monsters', 'tasks_master')).data;
+
+    if (maps.length === 0) {
+      logger.error(`Cannot find the tasks master. This shouldn't happen ??`);
+      return;
+    }
+
+    const contentLocation = this.character.evaluateClosestMap(maps);
+
+    await this.character.move({ x: contentLocation.x, y: contentLocation.y });
+
+    const response = await actionAcceptNewTask(this.character.data);
+
+    if (response instanceof ApiError) {
+      if (response.error.code === 499) {
+        logger.warn(`Character is in cooldown. [Code: ${response.error.code}]`);
+        await sleep(this.character.data.cooldown, 'cooldown');
+      }
+      // ToDo: Handle this somehow
+    } else {
+      this.character.data = response.data.character;
+    }
+  }
+}
