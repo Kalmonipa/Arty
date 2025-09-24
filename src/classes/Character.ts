911c27145a44e17208a87542fdfad67b82f9d769
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-09-24 21:28:25 +0200

    dump everything if we get a 497

--- src/classes/Character.ts
+++ src/classes/Character.ts
@@ -1501,9 +1501,9 @@ export class Character {
       case 493: // Character's level is too low
         // ToDo: Maybe train the skill to the required level?
         return false;
-      case 497: // The character's inventory is full.
+      case 497: // The character's inventory is full. Dump everything
         await this.evaluateDepositItemsInBank(
-          this.itemsToKeep,
+          undefined,
           { x: this.data.x, y: this.data.y },
           true,
         );
