e9f3ad21d0163da1cdc4c01b2dfd03a67639562a
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-10-16 08:48:10 +0200

    task spawned from TradeWithNPC wasn't getting the parent ID

--- src/objectives/TradeWithNPCObjective.ts
+++ src/objectives/TradeWithNPCObjective.ts
@@ -104,11 +104,11 @@ export class TradeObjective extends Objective {
         )
           if (Math.floor(Math.random() * 2) === 0) {
             await this.character.executeJobNow(
-              new MonsterTaskObjective(this.character, 1),
+              new MonsterTaskObjective(this.character, 1,), true, true, this.objectiveId
             );
           } else {
             await this.character.executeJobNow(
-              new ItemTaskObjective(this.character, 1),
+              new ItemTaskObjective(this.character, 1), true, true, this.objectiveId
             );
           }
       } else {
