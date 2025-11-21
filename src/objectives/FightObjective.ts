5587224f70fc6eef155f51987fe49e9e4d314654
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-11-22 00:28:14 +0100

    debug logs in the potion selection

--- src/objectives/FightObjective.ts
+++ src/objectives/FightObjective.ts
@@ -61,7 +61,10 @@ export class FightObjective extends Objective {
             potion.craft.level <= this.character.getCharacterLevel()
           ) {
             potionNeeded = potion.code;
+            logger.debug(`Chose to equip ${potion.code}`)
             break;
+          } else {
+            logger.debug(`${potion.code} is too high level or cannot be crafted`)
           }
         }
         fakeSchema.utility1_slot = potionNeeded;
