1d4c658fcb279e4265878aee981f17de51a2beea
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-12-11 22:23:27 +0100

    add headers

--- src/routes/Character.ts
+++ src/routes/Character.ts
@@ -44,15 +44,14 @@ export default function CharacterRouter(char: Character) {
         character: char.data.name,
         idle: char.isIdle,
         jobsInQueue: char.jobList.length,
-        currentJob: char.activeJob || 'none'
+        currentJob: char.activeJob || 'none',
       });
-    }
-  catch (error) {
+    } catch (error) {
       return res
         .status(500)
         .json({ error: error.message || 'Internal server error.' });
     }
-})
+  });
 
   return router;
 }
