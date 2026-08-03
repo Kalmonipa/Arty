1fa6bce1a996a0216c2cfb5e792c96565f544aa2
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2026-08-03 16:21:01 +0200

    missed onhold in /info

--- src/character/routes.ts
+++ src/character/routes.ts
@@ -60,6 +60,7 @@ export default function CharacterRouter(char: Character) {
         },
         numJobsInQueue: char.jobList.length,
         jobs: char.listObjectivesWithParents(),
+        onHold: char.listOnHoldJobs(),
         itemsToKeep: char.itemsToKeep,
       });
     } catch (error) {
